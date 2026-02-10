0f6673e470d3e19a2a0f78873ef2c332ddb2b7ed
Avraham Krol <122485458+AvrahamKrol@users.noreply.github.com>
2026-02-10 10:05:23 +0200

    added colors to ghosts
    Color of Ghosts changes depending on isSuper state of pacman

--- js/game.js
+++ js/game.js
@@ -1,56 +1,61 @@
-'use strict'
+'use strict';
 
-const WALL = '&#8251;'
-const FOOD = '&middot;'
-const EMPTY = ' '
+const WALL = '&#8251;';
+const FOOD = '&middot;';
+const SUPER = '🍯';
+const EMPTY = ' ';
 
 const gGame = {
-    score: 0,
-    isOn: false
-}
-var gBoard
+  score: 0,
+  isOn: false,
+};
+var gBoard;
 
 function init() {
-    console.log('hello')
+  console.log('hello');
 
-    gBoard = buildBoard()
-    createPacman(gBoard)
-    createGhosts(gBoard)
-    
-    renderBoard(gBoard, '.board-container')
-    gGame.isOn = true
+  gBoard = buildBoard();
+  createPacman(gBoard);
+  createGhosts(gBoard);
+
+  renderBoard(gBoard, '.board-container');
+  gGame.isOn = true;
 }
 
 function buildBoard() {
-    const size = 10
-    const board = []
+  const size = 10;
+  const board = [];
 
-    for (var i = 0; i < size; i++) {
-        board.push([])
+  for (var i = 0; i < size; i++) {
+    board.push([]);
 
-        for (var j = 0; j < size; j++) {
-            board[i][j] = FOOD
-            
-            if (i === 0 || i === size - 1 ||
-                j === 0 || j === size - 1 ||
-                (j === 3 && i > 4 && i < size - 2)) {
-                board[i][j] = WALL
-            }
-        }
+    for (var j = 0; j < size; j++) {
+      board[i][j] = FOOD;
+
+      if (
+        i === 0 ||
+        i === size - 1 ||
+        j === 0 ||
+        j === size - 1 ||
+        (j === 3 && i > 4 && i < size - 2)
+      ) {
+        board[i][j] = WALL;
+      }
     }
-    return board
+  }
+  return board;
 }
 
 function updateScore(diff) {
-    // update model
-    gGame.score += diff
-    
-    // update DOM
-    const elScore = document.querySelector('.score span')
-    elScore.innerText = gGame.score
+  // update model
+  gGame.score += diff;
+
+  // update DOM
+  const elScore = document.querySelector('.score span');
+  elScore.innerText = gGame.score;
 }
 
 function gameOver() {
-    console.log('Game Over')
-    gGame.isOn = false
+  console.log('Game Over');
+  gGame.isOn = false;
 }
